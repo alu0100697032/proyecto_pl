6573ebb542154076fdacf80d473c97fddba94291
Victor Hernandez Perez <alu0100697032.edu.es>
2015-04-09 21:25:27 +0000

    Added grammar path

--- routes/gram.js
+++ routes/gram.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('gram', { title: 'ADPR grammar' });
 });
 
 module.exports = router;
