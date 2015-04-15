2b0f19b1da878215174f1b338e895a8871a721d7
alu0100837353 <alu0100837353@ull.edu.es>
2015-04-15 12:00:15 +0000

    Testing working fine including covering with blanket

--- test/test.js
+++ test/test.js
@@ -14,11 +14,35 @@ suite('ADPR', function() {
             original.value="a=2+2";
             assert.deepEqual(original.value,"a=2+2");
         });
+        test('Recognize output from a sentence', function() {
+            original.value="a=2+2";
+            main();
+            assert.deepEqual(OUTPUT.innerHTML,'{\n  "type": "=",\n  "left": {\n    "type": "ID",\n    "value": "a"\n  },\n  "right": {\n    "type": "+",\n    "left": {\n      "type": "NUM",\n      "value": 2\n    },\n    "right": {\n      "type": "NUM",\n      "value": 2\n    }\n  }\n}');
+        });
+        test('Test example file 1', function() {
+            original.value="if a == 1 then b = 4";
+            main();
+            assert.deepEqual(OUTPUT.innerHTML,'{\n  "type": "IF",\n  "left": {\n    "type": "==",\n    "left": {\n      "type": "ID",\n      "value": "a"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 1\n    }\n  },\n  "right": {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "b"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 4\n    }\n  }\n}');
+        });
+        test('Test example file 2', function() {
+            original.value="a = 4 - 2 - 3";
+            main();
+            assert.deepEqual(OUTPUT.innerHTML,'{\n  "type": "=",\n  "left": {\n    "type": "ID",\n    "value": "a"\n  },\n  "right": {\n    "type": "-",\n    "left": {\n      "type": "-",\n      "left": {\n        "type": "NUM",\n        "value": 4\n      },\n      "right": {\n        "type": "NUM",\n        "value": 2\n      }\n    },\n    "right": {\n      "type": "NUM",\n      "value": 3\n    }\n  }\n}');
+        });
+        test('Test example file 3', function() {
+            original.value="a = ((4 / 2) / 3)";
+            main();
+            assert.deepEqual(OUTPUT.innerHTML,'{\n  "type": "=",\n  "left": {\n    "type": "ID",\n    "value": "a"\n  },\n  "right": {\n    "type": "/",\n    "left": {\n      "type": "/",\n      "left": {\n        "type": "NUM",\n        "value": 4\n      },\n      "right": {\n        "type": "NUM",\n        "value": 2\n      }\n    },\n    "right": {\n      "type": "NUM",\n      "value": 3\n    }\n  }\n}');
+        });
         test('Recognize ID in a sentence', function() {
             original.value="a=2+2";
             main();
             assert.match(OUTPUT.innerHTML,/ID/);
-            
+        });
+        test('Recognize NUM in a sentence', function() {
+            original.value="a=2+2";
+            main();
+            assert.match(OUTPUT.innerHTML,/NUM/);
         });
     });
     
