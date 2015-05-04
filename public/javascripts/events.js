d40388c2e26b9683f9b00454c688bc8fca5fd779
Victor Hernandez Perez <alu0100697032.edu.es>
2015-05-04 09:59:34 +0000

    Parsing with pegjs done

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
+    $('#PARSE').click(function() {
+        try {
+          var result = pl0.parse($('#original').val());
+          $('#OUTPUT').html(JSON.stringify(result,undefined,2));
+        } catch (e) {
+          $('#OUTPUT').html('<div class="error"><pre>\n' + String(e) + '\n</pre></div>');
+        }
+    });
+    //botones con ejemplos
     $("#ejemplo1").click(function(){
-       $("#original").val("if a == 1 then b = 4"); 
+       $("#original").val("if a == 1 then b = 4."); 
     });
     $("#ejemplo2").click(function(){
-       $("#original").val("a = 4 - 2 - 3"); 
+       $("#original").val("a = 4 - 2 - 3."); 
     });
     $("#ejemplo3").click(function(){
-       $("#original").val("a = ((4 / 2) / 3)"); 
+       $("#original").val("a = 4 / 2 / 3."); 
     });
     $("#error").click(function(){
-       $("#original").val("a = 3 + (4;"); 
+       $("#original").val("a = 3 + (4."); 
     });
 });
