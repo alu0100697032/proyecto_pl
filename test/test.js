7f7d3e9355cc02046b772381587fa9226f91a1fa
alu0100837353 <alu0100837353@ull.edu.es>
2015-05-13 17:15:38 +0000

    Added a lot of test

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ suite('ADPR', function() {
             original = document.getElementById('original');
             output = document.getElementById('OUTPUT');
             
+            
         }
     });
     suite('Testing', function() {
@@ -16,39 +17,163 @@ suite('ADPR', function() {
             assert.deepEqual(original.value,"a=2+2");
         });
         test('Recognize output from a sentence', function() {
-            var x=date.parse("a=2+2.");
-            assert.deepEqual(x.block.st.right.type, "+");
+            var x=parser.parse("25 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: lunes');
         });
-        test('Test example file 1', function() {
-            var x=date.parse("a = 4 - 2 - 3.");
-            assert.deepEqual(x.block.st.right.type,"-");
+        test('Monday', function() {
+            var x=parser.parse("25 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: lunes');
         });
-        test('Test example file 2', function() {
-            var x=date.parse("a = ((4 / 2) / 3).");
-            assert.deepEqual(x.block.st.right.type,"/");
+        test('Tuesday', function() {
+            var x=parser.parse("26 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: martes');
         });
-        test('Test and error', function() {
+        test('Wenesday', function() {
+            var x=parser.parse("27 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: miercoles');
+        });
+        test('Thrusday', function() {
+            var x=parser.parse("28 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: jueves');
+        });
+        test('Friday', function() {
+            var x=parser.parse("29 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: viernes');
+        });
+        test('Saturday', function() {
+            var x=parser.parse("30 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: sabado');
+        });
+        test('Sunday', function() {
+            var x=parser.parse("31 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: domingo');
+        });
+        test('january', function() {
+            var x=parser.parse("1 de enero de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: jueves');
+        });
+        test('february', function() {
+            var x=parser.parse("1 de febrero de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: domingo');
+        });
+        test('march', function() {
+            var x=parser.parse("1 de marzo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: domingo');
+        });
+        test('april', function() {
+            var x=parser.parse("1 de abril de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: miercoles');
+        });
+        test('may', function() {
+            var x=parser.parse("1 de mayo de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: viernes');
+        });
+        test('june', function() {
+            var x=parser.parse("1 de junio de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: lunes');
+        });
+        test('july', function() {
+            var x=parser.parse("1 de julio de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: miercoles');
+        });
+        test('august', function() {
+            var x=parser.parse("1 de agosto de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: sabado');
+        });
+        test('september', function() {
+            var x=parser.parse("1 de septiembre de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: martes');
+        });
+        test('october', function() {
+            var x=parser.parse("1 de octubre de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: jueves');
+        });
+        test('november', function() {
+            var x=parser.parse("1 de noviembre de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: domingo');
+        });
+        test('december', function() {
+            var x=parser.parse("1 de diciembre de 2015");
+            assert.deepEqual(x, 'Tipo de año: normal. Día de la semana: martes');
+        });
+        test('lep-year', function() {
+            var x=parser.parse("1 de enero de 2016");
+            assert.deepEqual(x, 'Tipo de año: bisiesto. Día de la semana: viernes');
+        });
+        test('empty', function() {
             try {
-            var x=date.parse("a = 3 + (4;.");
-            } catch (e) {
-                assert.deepEqual(String(e),'SyntaxError: Expected ")", [ \\t\\n\\r], [*\\/], [+\\-] or [0-9] but ";" found.');
+                var x=parser.parse("");
+            }
+            catch (err) {
+                assert;
             }
         });
-        test('Test and undefined', function() {
+        test('fail', function() {
             try {
-            var x=date.parse("     ");
-            } catch (e) {
-                assert.deepEqual(String(e),'SyntaxError: Expected "begin", "call", "const", "if", "procedure", "var", "while", [ \\t\\n\\r] or [a-zA-Z_] but end of input found.');
+                var x=parser.parse("1 de enro de 2016");
+            }
+            catch (err) {
+                assert;
             }
         });
-        test('Test a ID', function() {
-            var x=date.parse("a=hola.");
-            assert.deepEqual(x.block.st.right.type,"ID");
+        test('not day', function() {
+            try {
+                var x=parser.parse("enero de 2016");
+            }
+            catch (err) {
+                assert;
+            }
         });
-        test('Test a *', function() {
-            var x=date.parse("a=2*4.");
-            assert.deepEqual(x.block.st.right.type,"*");
+        test('not month', function() {
+            try {
+                var x=parser.parse("1 de 2016");
+            }
+            catch (err) {
+                assert;
+            }
         });
+        test('not year', function() {
+            try {
+                var x=parser.parse("1 de enro de 2016");
+            }
+            catch (err) {
+                assert;
+            }
+        });
+        test('invalid day', function() {
+            try {
+                var x=parser.parse("45 de enro de 2016");
+            }
+            catch (err) {
+                assert;
+            }
+        });
+        test('invalid day II', function() {
+            try {
+                var x=parser.parse("0 de enro de 2016");
+            }
+            catch (err) {
+                assert;
+            }
+        });
+        test('invalid year', function() {
+            try {
+                var x=parser.parse("1 de enro de 3450");
+            }
+            catch (err) {
+                assert;
+            }
+        });
+        test('invalid year II', function() {
+            try {
+                var x=parser.parse("1 de enro de 1450");
+            }
+            catch (err) {
+                assert;
+            }
+        });
+        
+        
     });
     
 });
