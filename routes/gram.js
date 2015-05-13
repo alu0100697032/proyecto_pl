01b887125ddc97a5e5d62454f86056e2307afbdb
Victor Hernandez Perez <alu0100697032.edu.es>
2015-05-13 17:47:34 +0000

    Some changes

--- routes/gram.js
+++ routes/gram.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('gram', { title: 'ADPR grammar' });
+  res.render('gram', { title: 'PL Proyect grammar' });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ADPR' });
+  res.render('index', { title: 'PL Proyect: Dates' });
 });
 
 module.exports = router;
