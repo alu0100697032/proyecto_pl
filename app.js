3fe6b1f9e084631bfd661ab38d5bed22e56831c9
Victor Hernandez Perez <alu0100697032.edu.es>
2015-05-13 11:31:52 +0000

    Bug fixed

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var gram = require('./routes/gram');
-var test = require('./routes/test');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/gram', gram);
-app.use('/test', test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
