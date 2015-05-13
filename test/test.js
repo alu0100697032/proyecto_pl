7e6a4cd87826c03dfa94e161bb3109812eef1c08
alu0100837353 <alu0100837353@ull.edu.es>
2015-05-13 12:28:34 +0000

    Fixing html and starting update of test.js

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ suite('ADPR', function() {
             document.body.innerHTML = __html__['test.html'];
             original = document.getElementById('original');
             output = document.getElementById('OUTPUT');
+            
         }
     });
     suite('Testing', function() {
@@ -15,49 +16,38 @@ suite('ADPR', function() {
             assert.deepEqual(original.value,"a=2+2");
         });
         test('Recognize output from a sentence', function() {
-            original.value="a=2+2";
-            main();
-            assert.deepEqual(OUTPUT.innerHTML,'{\n  "type": "=",\n  "left": {\n    "type": "ID",\n    "value": "a"\n  },\n  "right": {\n    "type": "+",\n    "left": {\n      "type": "NUM",\n      "value": 2\n    },\n    "right": {\n      "type": "NUM",\n      "value": 2\n    }\n  }\n}');
+            var x=date.parse("a=2+2.");
+            assert.deepEqual(x.block.st.right.type, "+");
         });
         test('Test example file 1', function() {
-            original.value="if a == 1 then b = 4";
-            main();
-            assert.deepEqual(OUTPUT.innerHTML,'{\n  "type": "IF",\n  "left": {\n    "type": "==",\n    "left": {\n      "type": "ID",\n      "value": "a"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 1\n    }\n  },\n  "right": {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "b"\n    },\n    "right": {\n      "type": "NUM",\n      "value": 4\n    }\n  }\n}');
+            var x=date.parse("a = 4 - 2 - 3.");
+            assert.deepEqual(x.block.st.right.type,"-");
         });
         test('Test example file 2', function() {
-            original.value="a = 4 - 2 - 3";
-            main();
-            assert.deepEqual(OUTPUT.innerHTML,'{\n  "type": "=",\n  "left": {\n    "type": "ID",\n    "value": "a"\n  },\n  "right": {\n    "type": "-",\n    "left": {\n      "type": "-",\n      "left": {\n        "type": "NUM",\n        "value": 4\n      },\n      "right": {\n        "type": "NUM",\n        "value": 2\n      }\n    },\n    "right": {\n      "type": "NUM",\n      "value": 3\n    }\n  }\n}');
-        });
-        test('Test example file 3', function() {
-            original.value="a = ((4 / 2) / 3)";
-            main();
-            assert.deepEqual(OUTPUT.innerHTML,'{\n  "type": "=",\n  "left": {\n    "type": "ID",\n    "value": "a"\n  },\n  "right": {\n    "type": "/",\n    "left": {\n      "type": "/",\n      "left": {\n        "type": "NUM",\n        "value": 4\n      },\n      "right": {\n        "type": "NUM",\n        "value": 2\n      }\n    },\n    "right": {\n      "type": "NUM",\n      "value": 3\n    }\n  }\n}');
+            var x=date.parse("a = ((4 / 2) / 3).");
+            assert.deepEqual(x.block.st.right.type,"/");
         });
         test('Test and error', function() {
-            original.value="a = 3 + (4;";
-            main();
-            assert.deepEqual(OUTPUT.innerHTML,'<div class="error">Syntax Error. Expected ) found \';\' near \';\'</div>');
+            try {
+            var x=date.parse("a = 3 + (4;.");
+            } catch (e) {
+                assert.deepEqual(String(e),'SyntaxError: Expected ")", [ \\t\\n\\r], [*\\/], [+\\-] or [0-9] but ";" found.');
+            }
         });
         test('Test and undefined', function() {
-            original.value="    ";
-            main();
-            assert.deepEqual(OUTPUT.innerHTML,'<div class="error">TypeError: Cannot read property \'from\' of undefined</div>');
+            try {
+            var x=date.parse("     ");
+            } catch (e) {
+                assert.deepEqual(String(e),'SyntaxError: Expected "begin", "call", "const", "if", "procedure", "var", "while", [ \\t\\n\\r] or [a-zA-Z_] but end of input found.');
+            }
         });
-        test('Test a string', function() {
-            original.value="hola";
-            main();
-            assert.deepEqual(OUTPUT.innerHTML,'<div class="error">TypeError: Cannot read property \'type\' of null</div>');
+        test('Test a ID', function() {
+            var x=date.parse("a=hola.");
+            assert.deepEqual(x.block.st.right.type,"ID");
         });
-        test('Recognize ID in a sentence', function() {
-            original.value="a=2+2";
-            main();
-            assert.match(OUTPUT.innerHTML,/ID/);
-        });
-        test('Recognize NUM in a sentence', function() {
-            original.value="a=2+2";
-            main();
-            assert.match(OUTPUT.innerHTML,/NUM/);
+        test('Test a *', function() {
+            var x=date.parse("a=2*4.");
+            assert.deepEqual(x.block.st.right.type,"*");
         });
     });
     
